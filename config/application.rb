44d1e431d00b41f66fa1017dc913ee0af4d0db8f
Rafi Khan <rafi.fa.khan@gmail.com>
2014-12-09 12:56:12 -0500

    contact directive

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Marketplace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Heroku serve assets
+    config.serve_static_assets = true
   end
 end
