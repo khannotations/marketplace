7c3b749ff45802ddcae51d87ec4e2fcebea12e52
Rafi Khan <rafi.fa.khan@gmail.com>
2015-02-18 17:09:53 -0500

    minor bug fixes

--- app/assets/javascripts/angular/directives/contact.js
+++ app/assets/javascripts/angular/directives/contact.js
@@ -24,9 +24,11 @@ angular.module("Marketplace").directive("contact",
               // Create an actual resource
               scope.projectResource = new Project(scope.project);
               scope.projectResource.$contact(function() {
-                scope.$emit("flash", {state: "success",
-                  msg: "The project's leaders were contacted! Check your email to follow up.<br>" + 
-                  "We've also added this project to your list of starred projects."});
+                scope.$emit("flash", {
+                  state: "success",
+                  msg: "The project's leaders were contacted—" + 
+                    "check your email to follow up!"
+                });
               });
             }
             $("#wrapper").removeClass("blur");
